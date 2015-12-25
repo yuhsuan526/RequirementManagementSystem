161148f51cb9f0ffbea30ec8cdb61369a7adba93
YH <t104598001@ntut.edu.tw>
2015-12-25 17:55:33 +0800

    新增 commentEditorForm

---
+++ RMS_Project/RMS_Project/RMIAS/CommentEditorForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace RMS_Project.RMIAS
+{
+    partial class CommentEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.DescriptionRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.MemoRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.DescriptionRichTextBox, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.MemoRichTextBox, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.confirmButton, 3, 5);
+            this.tableLayoutPanel1.Controls.Add(this.versionLabel, 3, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(50, 20, 50, 20);
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(650, 492);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(53, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Requirement";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(345, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 26);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Owner";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(53, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 26);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Comment";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Location = new System.Drawing.Point(53, 239);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 26);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Decision";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // DescriptionRichTextBox
+            // 
+            this.DescriptionRichTextBox.AccessibleName = "descriptionLabel";
+            this.tableLayoutPanel1.SetColumnSpan(this.DescriptionRichTextBox, 4);
+            this.DescriptionRichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DescriptionRichTextBox.Location = new System.Drawing.Point(53, 75);
+            this.DescriptionRichTextBox.Name = "DescriptionRichTextBox";
+            this.DescriptionRichTextBox.Size = new System.Drawing.Size(544, 161);
+            this.DescriptionRichTextBox.TabIndex = 19;
+            this.DescriptionRichTextBox.Text = "";
+            // 
+            // MemoRichTextBox
+            // 
+            this.MemoRichTextBox.AccessibleName = "memoLabel";
+            this.tableLayoutPanel1.SetColumnSpan(this.MemoRichTextBox, 4);
+            this.MemoRichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MemoRichTextBox.Location = new System.Drawing.Point(53, 268);
+            this.MemoRichTextBox.Name = "MemoRichTextBox";
+            this.MemoRichTextBox.Size = new System.Drawing.Size(544, 161);
+            this.MemoRichTextBox.TabIndex = 18;
+            this.MemoRichTextBox.Text = "";
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.AccessibleName = "confirmButton";
+            this.confirmButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.confirmButton.Location = new System.Drawing.Point(390, 435);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(207, 34);
+            this.confirmButton.TabIndex = 5;
+            this.confirmButton.Text = "confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.versionLabel.Location = new System.Drawing.Point(389, 20);
+            this.versionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(209, 26);
+            this.versionLabel.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(132, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 26);
+            this.label3.TabIndex = 24;
+            // 
+            // CommentEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 492);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "CommentEditorForm";
+            this.Text = "CommentEditorForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox DescriptionRichTextBox;
+        private System.Windows.Forms.RichTextBox MemoRichTextBox;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.Label label3;
+    }
+}
